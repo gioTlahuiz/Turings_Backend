c1e367070df3dc3db2d7f787368fe2b37dfb9705
Oswaldo <santoswaldos@gmail.com>
2026-05-12 14:36:18 -0600

    Integrando los métodos CRUD para la manipulación de datos de los detalles de pedidos

--- src/main/java/com/turings/backend/service/DetallesPedidosService.java
+++ src/main/java/com/turings/backend/service/DetallesPedidosService.java
@@ -1,4 +1,46 @@
 package com.turings.backend.service;
 
+import com.turings.backend.model.DetallesPedidos;
+import com.turings.backend.repository.DetallesPedidosRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/***
+ * @version 1.0
+ * @since 1.0
+ */
+
 public class DetallesPedidosService {
+
+    final private DetallesPedidosRepository detallesPedidosRepository;
+
+    public DetallesPedidosService(DetallesPedidosRepository detallesPedidosRepository) {
+        this.detallesPedidosRepository = detallesPedidosRepository;
+    }
+
+    //Metodo para encontrar todo los detalles de producto
+    public List<DetallesPedidos> findAll() {
+        return detallesPedidosRepository.findAll();
+    }
+
+    //Metodo para encontrar detalle de producto por ID
+    public Optional<DetallesPedidos> findById(Long id) {
+        return detallesPedidosRepository.findById(id);
+    }
+
+    //Metodo para guardar detalles de productos
+    public DetallesPedidos save(DetallesPedidos detallesPedidos) {
+        return detallesPedidosRepository.save(detallesPedidos);
+    }
+
+    //Metodo para eliminar detalles de productos por ID
+    public Optional<DetallesPedidos> deleteById(Long id) {
+        Optional<DetallesPedidos> detallesPedidosOptional = detallesPedidosRepository.findById(id);
+        if (detallesPedidosOptional.isPresent()) {
+            detallesPedidosRepository.deleteById(id);
+            return detallesPedidosOptional;
+        }
+        return Optional.empty();
+    }
 }
