4aa8e1e5da1a05e79474675fb421626ddd861add
Jesus <jesusmoralesgarita@gmail.com>
2026-05-12 15:55:16 -0600

    Se agregó las peticiones http a usuario

--- src/main/java/com/turings/backend/controller/UsuarioController.java
+++ src/main/java/com/turings/backend/controller/UsuarioController.java
@@ -1,4 +1,42 @@
 package com.turings.backend.controller;
 
+import com.turings.backend.model.Usuario;
+import com.turings.backend.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/v1/usuario")
 public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public UsuarioController (UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<Usuario> getAllUsuarios(){
+        return usuarioService.getAllUsuarios();
+    }
+
+    @PostMapping
+    public Usuario guardarUsuario (@RequestBody Usuario usuario){
+        return usuarioService.guardarUsuario(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario actualizarUsuario (@PathVariable Long id, @RequestBody Usuario usuario) {
+        return usuarioService.actualizarUsuario(id, usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void borrarUsuario (@PathVariable Long id) {
+        usuarioService.borrarUsuario(id);
+    }
 }
