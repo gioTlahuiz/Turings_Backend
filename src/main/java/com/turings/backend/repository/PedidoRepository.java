26c8fc2c9510ff7d322e678806e698c34c1ddf32
Ferscho <fschespkr@gmail.com>
2026-05-14 10:45:47 -0600

    agregando PedidoController y PedidoService

--- src/main/java/com/turings/backend/repository/PedidoRepository.java
+++ src/main/java/com/turings/backend/repository/PedidoRepository.java
@@ -4,6 +4,6 @@ import com.turings.backend.model.Categoria;
 import com.turings.backend.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
     Pedido findById(long id);
 }
