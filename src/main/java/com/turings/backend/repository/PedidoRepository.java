6abd0a3662016e7b2dfc0b295e87f758f8e0ad0f
Ferscho <fschespkr@gmail.com>
2026-05-14 13:08:26 -0600

    corrigiendo

--- src/main/java/com/turings/backend/repository/PedidoRepository.java
+++ src/main/java/com/turings/backend/repository/PedidoRepository.java
@@ -5,5 +5,5 @@ import com.turings.backend.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
-    Pedido findById(long id);
+    //Pedido findById(long id);
 }
