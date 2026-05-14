2c11d9a6a6f0e81e9c3e859fce58b58ebb21a609
gioTlahuiz <fcei.tlahuiz.2.e@gmail.com>
2026-05-14 13:34:43 -0600

    se modifico el requestmapping

--- src/main/java/com/turings/backend/controller/CategoriaController.java
+++ src/main/java/com/turings/backend/controller/CategoriaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/categorias")
+@RequestMapping("/api/v1/categorias")
 public class CategoriaController {
 
     private final CategoriaService servCategoria;
