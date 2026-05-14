77ed51332615f2f3d9e714ab894dcc585df0c699
Luis <LuiCaba@outlook.es>
2026-05-14 16:46:29 -0600

    se corrigieron los controladores

--- src/main/java/com/turings/backend/controller/ReviewController.java
+++ src/main/java/com/turings/backend/controller/ReviewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reviews")
+@RequestMapping("/api/v1/reviews")
 @CrossOrigin(origins = "*")
 public class ReviewController {
 
