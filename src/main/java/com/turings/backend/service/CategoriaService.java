80e24596b58790e742ef922070dd457439e0d9a1
gioTlahuiz <fcei.tlahuiz.2.e@gmail.com>
2026-05-14 13:15:07 -0600

    se agrego el controller y service de categorias

--- src/main/java/com/turings/backend/service/CategoriaService.java
+++ src/main/java/com/turings/backend/service/CategoriaService.java
@@ -1,4 +1,50 @@
 package com.turings.backend.service;
 
+import com.turings.backend.model.Categoria;
+import com.turings.backend.repository.CategoriaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Service
 public class CategoriaService {
+
+    private final CategoriaRepository repoCategorias;
+
+    public CategoriaService(CategoriaRepository repoCategorias) {
+        this.repoCategorias = repoCategorias;
+    }
+
+    // Obtener categoría
+    public List<Categoria> obtenerCategorias(){
+
+        return repoCategorias.findAll();
+    }
+
+    // Crear categoría
+    public Categoria crearCategoria(Categoria categoria){
+
+        return repoCategorias.save(categoria);
+    }
+
+    // Actualizar categoría
+    public Categoria guardarCategoria(Long id, Categoria nuevaCategoria){
+
+        Categoria categoriaExistente = repoCategorias.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Categoria no encontrada"));
+
+        categoriaExistente.setCategoria(
+                nuevaCategoria.getCategoria()
+        );
+
+        return repoCategorias.save(categoriaExistente);
+    }
+
+    // Eliminar categoría
+    public void eliminarCategoria(Long id){
+
+        repoCategorias.deleteById(id);
+    }
 }
