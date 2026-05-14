6863392a416afc85a537eabaff62fe7ba046b80d
EMGA <erickmgarciaa@outlook.com>
2026-05-14 17:14:44 -0600

    checkeo

--- src/main/java/com/turings/backend/controller/CategoriaController.java
+++ src/main/java/com/turings/backend/controller/CategoriaController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/categorias")
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 public class CategoriaController {
 
     private final CategoriaService servCategoria;
