4018739eb0383e845bc345294cbad75437ac3e6f
Luis <LuiCaba@outlook.es>
2026-05-28 13:52:44 -0600

    se mejoro el controlador de reviews

--- src/main/java/com/turings/backend/service/ReviewService.java
+++ src/main/java/com/turings/backend/service/ReviewService.java
@@ -3,7 +3,11 @@ package com.turings.backend.service;
 import com.turings.backend.model.Review;
 import com.turings.backend.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -14,7 +18,8 @@ public class ReviewService {
 
 
     public List<Review> getAllReviews() {
-        return reviewRepository.findAll();
+        Pageable page = PageRequest.of(0, 6, Sort.by("idReviews").descending());
+        return reviewRepository.findAll(page).toList();
     }
 
 
